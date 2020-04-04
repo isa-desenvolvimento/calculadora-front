b958dcd9d25c41d1cdd92f62117302e748ba7119
Andressa Menezes <izzaandressa@gmail.com>
2020-04-04 15:47:30 -0300

    table cheque mock

--- src/app/_component/pages/cheque-empresarial/cheque-empresarial.component.ts
+++ src/app/_component/pages/cheque-empresarial/cheque-empresarial.component.ts
@@ -1,44 +1,134 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChequeEmpresarial } from '../../../_models/ChequeEmpresarial';
 
+declare interface TableData {
+  headerRow: Array<Object>;
+  dataRows: Array<Object>;
+}
 
 @Component({
-    selector: 'cheque-empresarial-cmp',
-    moduleId: module.id,
-    templateUrl: 'cheque-empresarial.component.html'
+  selector: 'cheque-empresarial-cmp',
+  moduleId: module.id,
+  templateUrl: 'cheque-empresarial.component.html'
 })
 
-export class ChequeEmpresarialComponent implements OnInit{
+export class ChequeEmpresarialComponent implements OnInit {
 
   ceForm: FormGroup;
-
-
   loading = false;
   submitted = false;
   returnUrl: string;
   errorMessage = '';
   payload: ChequeEmpresarial;
+  tableData: TableData;
+  teste = 123
 
 
   constructor(
-    private formBuilder: FormBuilder,
-  ) {}
+  ) { }
 
   ngOnInit() {
-    this.ceForm = this.formBuilder.group({
-      ce_pasta: ['', Validators.required],
-      ce_contrato: ['', Validators.required],
-      ce_tipo_contrato: ['', Validators.required]
-    });
+    this.buildDataTable();
   }
 
-  // convenience getter for easy access to form fields
-  get f1() { return this.ceForm.controls; }
-
-
-
-  resetFields() {
-    this.ceForm.reset()
+  // TODO: Componentizar tabela e passar valores por @input e tratar eventos
+  buildDataTable() {
+    this.tableData = {
+      headerRow: [{
+        dataBase: {
+          title: "data Base",
+          nested: false
+        },
+        indiceDataBase: {
+          title: "indice Data Base",
+          nested: false
+        },
+        dataBaseAtual: {
+          title: "data Base Atual",
+          nested: false
+        },
+        indiceDataAtual: {
+          title: "indice Data Atual",
+          nested: false
+        },
+        valorDevedor: {
+          title: "valor Devedor",
+          nested: false
+        },
+        encargosMonetarios: {
+          title: "encargos Monetarios",
+          nested: false
+        },
+        correcaoPeloIndice: {
+          title: "correcao Pelo Indice",
+          nested: false
+        },
+        jurosAm: {
+          title: "juros Ao mês",
+          nested: false
+        },
+        dias: {
+          title: "dias",
+          nested: false
+        },
+        percents: {
+          title: "%",
+          nested: false
+        },
+        moneyValue: {
+          title: "R$",
+          nested: false
+        },
+        multa: {
+          title: "multa",
+          nested: false
+        },
+        lancamentos: {
+          title: "lançamentos",
+          nested: false
+        },
+        valorDevedorAtualizado: {
+          title: "valor Devedor Atualizado",
+          nested: false
+        }
+      }],
+      dataRows: [{
+        dataBase: "02/01/1997",
+        indiceDataBase: "INPC",
+        dataBaseAtual: "02/12/2010",
+        indiceDataAtual: "100",
+        valorDevedor: "233300",
+        encargosMonetarios: {
+          correcaoPeloIndice: "1233",
+          jurosAm: {
+            dias: "10",
+            percents: "2",
+            moneyValue: "1000000",
+          },
+          multa: "30000",
+        },
+        lancamentos: "131231",
+        valorDevedorAtualizado: "0"
+      },
+      {
+        dataBase: "02/01/1997",
+        indiceDataBase: "INPC",
+        dataBaseAtual: "02/12/2010",
+        indiceDataAtual: "100",
+        valorDevedor: "233300",
+        encargosMonetarios: {
+          correcaoPeloIndice: "1233",
+          jurosAm: {
+            dias: "10",
+            percents: "2",
+            moneyValue: "1000000",
+          },
+          multa: "30000",
+        },
+        lancamentos: "131231",
+        valorDevedorAtualizado: "0"
+      }]
+    };
   }
+
 }
