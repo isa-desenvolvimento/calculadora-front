efce801e740c0e5c21b716624cfbc8ec85fcc6a9
andressanovaes <andressa.novaes@immail.ca>
2020-07-12 14:54:48 -0300

    - Mostra parcelas pagas com outra cor

--- src/app/_component/pages/amortizacao/amortizacao.component.ts
+++ src/app/_component/pages/amortizacao/amortizacao.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { getCurrentDate, formatDate, formatCurrency, getLastLine, verifyNumber, getQtdDias } from '../../util/util';
-import { LANGUAGEM_TABLE, LISTA_AMORTIZACAO } from '../../util/constants'
+import { LANGUAGEM_TABLE, LISTA_AMORTIZACAO, AMORTIZACAO_DATA_ATUAL } from '../../util/constants'
 
 declare interface TableData {
   dataRows: Array<Object>;
@@ -16,7 +16,7 @@ export class AmortizacaoComponent implements OnInit {
   @Input() pesquisaFormValid: boolean;
   @Input() dataCalculo: string;
 
-  
+
   @Output() incluirAmortizacao = new EventEmitter();
   @Output() deleteAmortizacao = new EventEmitter();
 
@@ -77,6 +77,17 @@ export class AmortizacaoComponent implements OnInit {
 
 
   adicionarAmortizacao() {
+    const preFATipo = this.pre_form_amortizacao.tipo.value;
+
+    switch (preFATipo) {
+      case AMORTIZACAO_DATA_ATUAL:
+        this.preFormAmortizacao.value.data_vencimento = this.formatDate(this.dataCalculo, 'YYYY-MM-DD')
+        break;
+
+      default:
+        break;
+    }
+    
     this.tableDataAmortizacao.dataRows.push(this.preFormAmortizacao.value);
 
     this.incluirAmortizacao.emit(this.tableDataAmortizacao.dataRows)
