3603d7982df4c0ac9c59b30a7220bde4d23ea12f
andressanovaes <andressa.novaes@immail.ca>
2020-08-05 22:49:46 -0300

    - Cliente e CNPJ na pesquisa

--- src/app/_component/pages/pesquisa/pesquisa.component.ts
+++ src/app/_component/pages/pesquisa/pesquisa.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PastasContratosService } from '../../../_services/pastas-contratos.service';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { PastasContratosService } from "../../../_services/pastas-contratos.service";
 
 @Component({
-  selector: 'app-pesquisa',
-  templateUrl: './pesquisa.component.html'
+  selector: "app-pesquisa",
+  templateUrl: "./pesquisa.component.html",
 })
 export class PesquisaComponent implements OnInit {
-
   peForm: FormGroup;
-  txtContractRef: string = '';
+  txtContractRef: string = "";
 
   @Input() tableLoading: boolean;
   @Output() contractRef = new EventEmitter();
@@ -17,20 +16,23 @@ export class PesquisaComponent implements OnInit {
 
   contractList_field = [];
   typeContractList_field = [];
-  folderData_field = this.agruparPasta();
+  cnpjList_field = this.agruparPasta("CNPJ");
+  clienteList_field = this.agruparPasta("CLIENTE");
+  folderData_field = this.agruparPasta("PASTA");
 
   constructor(
     private formBuilder: FormBuilder,
     private pastasContratosService: PastasContratosService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.peForm = this.formBuilder.group({
-      pasta: ['', Validators.required],
-      contrato: ['', Validators.required],
-      tipo_contrato: ['', Validators.required],
-      recuperacaoJudicial: [false, Validators.required]
+      pasta: ["", Validators.required],
+      contrato: ["", Validators.required],
+      tipo_contrato: ["", Validators.required],
+      recuperacaoJudicial: [false, Validators.required],
+      cliente: ["", Validators.required],
+      cnpj: ["", Validators.required],
     });
   }
 
@@ -39,25 +41,29 @@ export class PesquisaComponent implements OnInit {
     const contrato = this.form.contrato.value;
     const tipo_contrato = this.form.tipo_contrato.value;
     const recuperacaoJudicial = this.form.recuperacaoJudicial.value;
+    const cliente = this.form.cliente.value;
+    const cnpj = this.form.cnpj.value;
 
-    this.txtContractRef = pasta + contrato + tipo_contrato + recuperacaoJudicial;
+    this.txtContractRef =
+      pasta + contrato + tipo_contrato + recuperacaoJudicial;
     this.resetForm.emit(true);
     const info = {
       pasta,
       contrato,
       tipo_contrato,
       contractRef: this.txtContractRef,
-      recuperacaoJudicial
+      recuperacaoJudicial,
+      cliente,
+      cnpj,
     };
 
     this.contractRef.emit(info);
   }
 
-
-  agruparPasta() {
+  agruparPasta(attr) {
     let pastasFiltros = [];
 
-    this.pastas['data'].map(pasta => pastasFiltros.push(pasta.PASTA));
+    this.pastas["data"].map((pasta) => pastasFiltros.push(pasta[attr]));
     const setUnico = new Set(pastasFiltros);
 
     return [...setUnico];
@@ -67,30 +73,56 @@ export class PesquisaComponent implements OnInit {
     this.contractList_field = [];
     this.typeContractList_field = [];
 
-    if (this.form.contrato.value || this.form.tipo_contrato.value) {
-      this.peForm.reset({ pasta: this.form.pasta.value, recuperacaoJudicial: false })
+    if (!this.form.cliente.value) {
+      this.clienteList_field = [];
     }
 
-    this.pastas['data'].map(pasta => {
+    if (!this.form.cnpj.value) {
+      this.cnpjList_field = [];
+    }
+
+    if (this.form.contrato.value || this.form.tipo_contrato.value) {
+      this.peForm.reset({
+        pasta: this.form.pasta.value,
+        recuperacaoJudicial: false,
+      });
+    }
+
+    this.pastas["data"].map((pasta) => {
       if (pasta.PASTA === this.form.pasta.value) {
         this.contractList_field.push(pasta.CONTRATO);
+        this.clienteList_field.push(pasta.CLIENTE);
+        this.cnpjList_field.push(pasta.CNPJ);
       }
     });
 
     const setUnico = new Set(this.contractList_field);
     this.contractList_field = [...setUnico];
+
+    const setUnicoCliente = new Set(this.clienteList_field);
+    this.clienteList_field = [...setUnicoCliente];
+
+    const setUnicoCNPJ = new Set(this.cnpjList_field);
+    this.cnpjList_field = [...setUnicoCNPJ];
   }
 
   setTypeContract() {
     this.typeContractList_field = [];
-    this.peForm.value.tipo_contrato = '';
+    this.peForm.value.tipo_contrato = "";
 
     if (this.form.tipo_contrato.value) {
-      this.peForm.reset({ pasta: this.form.pasta.value, contrato: this.form.contrato.value, recuperacaoJudicial: false })
+      this.peForm.reset({
+        pasta: this.form.pasta.value,
+        contrato: this.form.contrato.value,
+        recuperacaoJudicial: false,
+      });
     }
 
-    this.pastas['data'].map(pasta => {
-      if (pasta.PASTA === this.form.pasta.value && pasta.CONTRATO === this.form.contrato.value) {
+    this.pastas["data"].map((pasta) => {
+      if (
+        pasta.PASTA === this.form.pasta.value &&
+        pasta.CONTRATO === this.form.contrato.value
+      ) {
         this.typeContractList_field.push(pasta.DESCRICAO);
       }
     });
@@ -99,6 +131,82 @@ export class PesquisaComponent implements OnInit {
     this.typeContractList_field = [...setUnico];
   }
 
-  get pastas() { return this.pastasContratosService.getPastas(); }
-  get form() { return this.peForm.controls; }
+  setCliente() {
+    if (!this.form.pasta.value) {
+      this.folderData_field = [];
+      this.contractList_field = [];
+      this.typeContractList_field = [];
+    }
+
+    if (!this.form.cnpj.value) {
+      this.cnpjList_field = [];
+    }
+
+    if (this.form.contrato.value || this.form.tipo_contrato.value) {
+      this.peForm.reset({
+        pasta: this.form.pasta.value,
+        recuperacaoJudicial: false,
+      });
+    }
+
+    this.pastas["data"].map((pasta) => {
+      if (pasta.CLIENTE === this.form.cliente.value) {
+        this.contractList_field.push(pasta.CONTRATO);
+        this.folderData_field.push(pasta.PASTA);
+        this.cnpjList_field.push(pasta.CNPJ);
+      }
+    });
+
+    const setUnico = new Set(this.contractList_field);
+    this.contractList_field = [...setUnico];
+
+    const setUnicoCliente = new Set(this.folderData_field);
+    this.folderData_field = [...setUnicoCliente];
+
+    const setUnicoCNPJ = new Set(this.cnpjList_field);
+    this.cnpjList_field = [...setUnicoCNPJ];
+  }
+
+  setCnpj() {
+    if (!this.form.pasta.value) {
+      this.folderData_field = [];
+      this.contractList_field = [];
+      this.typeContractList_field = [];
+    }
+
+    if (!this.form.cliente.value) {
+      this.clienteList_field = [];
+    }
+
+    if (this.form.contrato.value || this.form.tipo_contrato.value) {
+      this.peForm.reset({
+        pasta: this.form.pasta.value,
+        recuperacaoJudicial: false,
+      });
+    }
+
+    this.pastas["data"].map((pasta) => {
+      if (pasta.CNPJ === this.form.cnpj.value) {
+        this.contractList_field.push(pasta.CONTRATO);
+        this.clienteList_field.push(pasta.CLIENTE);
+        this.folderData_field.push(pasta.PASTA);
+      }
+    });
+
+    const setUnico = new Set(this.contractList_field);
+    this.contractList_field = [...setUnico];
+
+    const setUnicoCliente = new Set(this.clienteList_field);
+    this.clienteList_field = [...setUnicoCliente];
+
+    const setUnicoPasta = new Set(this.folderData_field);
+    this.folderData_field = [...setUnicoPasta];
+  }
+
+  get pastas() {
+    return this.pastasContratosService.getPastas();
+  }
+  get form() {
+    return this.peForm.controls;
+  }
 }
