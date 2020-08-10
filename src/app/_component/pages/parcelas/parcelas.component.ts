8968f2100a33295282c8fe516bcadd1fba56fdf3
andressanovaes <andressa.novaes@immail.ca>
2020-08-09 21:55:06 -0300

    - Ajuste

--- src/app/_component/pages/parcelas/parcelas.component.ts
+++ src/app/_component/pages/parcelas/parcelas.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import 'datatables.net';
-import 'datatables.net-buttons';
-import * as moment from 'moment'; // add this 1 of 4
+import "datatables.net";
+import "datatables.net-buttons";
+import * as moment from "moment"; // add this 1 of 4
 
-import { getCurrentDate, formatDate, formatCurrency, getLastLine, verifyNumber, getQtdDias } from '../../util/util';
-import { LANGUAGEM_TABLE, LISTA_STATUS } from '../../util/constants'
+import {
+  getCurrentDate,
+  formatDate,
+  formatCurrency,
+  getLastLine,
+  verifyNumber,
+  getQtdDias,
+} from "../../util/util";
+import {
+  LANGUAGEM_TABLE,
+  LISTA_STATUS,
+  LISTA_INDICES,
+} from "../../util/constants";
 
 declare interface TableData {
   dataRows: Array<Object>;
 }
 
 @Component({
-  selector: 'app-parcelas',
-  templateUrl: './parcelas.component.html'
+  selector: "app-parcelas",
+  templateUrl: "./parcelas.component.html",
 })
 export class ParcelasComponent implements OnInit {
-
   @Input() pesquisaFormValid: boolean;
   @Input() newParcelasTableClear: boolean;
   @Input() minParcela: number;
 
   @Output() incluirParcelas = new EventEmitter();
+
+  indice_field = LISTA_INDICES;
 
   status_field = LISTA_STATUS;
   preFormCadastroParcelas: FormGroup;
@@ -30,55 +42,53 @@ export class ParcelasComponent implements OnInit {
   tableLoading = false;
   updateLoading = false;
   alertType = {
-    mensagem: '',
-    tipo: ''
+    mensagem: "",
+    tipo: "",
   };
 
   tableDataParcelas: TableData;
   dtOptions: DataTables.Settings = {};
 
-  constructor(
-    private formBuilder: FormBuilder
-  ) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.tableDataParcelas = {
-      dataRows: []
-    }
+      dataRows: [],
+    };
 
     this.preFormCadastroParcelas = this.formBuilder.group({
-      nparcelas: ['', Validators.required],
+      nparcelas: ["", Validators.required],
       parcelaInicial: [[], Validators.required],
-      dataVencimento: ['', Validators.required],
-      valorNoVencimento: ['', Validators.required],
-      status: ['', Validators.required]
-    })
+      dataVencimento: ["", Validators.required],
+      valorNoVencimento: ["", Validators.required],
+      status: ["", Validators.required],
+    });
 
     this.dtOptions = {
       paging: true,
       searching: false,
-      language: LANGUAGEM_TABLE
-    }
+      language: LANGUAGEM_TABLE,
+    };
   }
 
   verifyNumber(value) {
-    verifyNumber(value)
+    verifyNumber(value);
   }
 
   formatCurrency(value) {
-    return formatCurrency(value)
+    return formatCurrency(value);
   }
 
-  formatDate(value, format = 'DD/MM/YYYY') {
-    return formatDate(value, format)
+  formatDate(value, format = "DD/MM/YYYY") {
+    return formatDate(value, format);
   }
 
   verifyNumberInitial(e) {
     e.preventDefault();
     if (this.minParcela > e.target.value) {
-      e.target.style.borderColor = '#ef8157'
-    }else {
-      e.target.style.borderColor = '#DDDDDD'
+      e.target.style.borderColor = "#ef8157";
+    } else {
+      e.target.style.borderColor = "#DDDDDD";
     }
   }
 
@@ -89,9 +99,19 @@ export class ParcelasComponent implements OnInit {
     this.tableDataParcelas.dataRows = [];
 
     let mes = 1;
-    for (let index = parcelaInicial; index < (nParcelas + parcelaInicial); index++) {
-      this.tableDataParcelas.dataRows.push({ ...this.preFormCadastroParcelas.value, nparcelas: index, dataVencimento: dataVencimento });
-      dataVencimento = moment(dataVencimento).add(mes, 'months').format('YYYY-MM-DD');
+    for (
+      let index = parcelaInicial;
+      index < nParcelas + parcelaInicial;
+      index++
+    ) {
+      this.tableDataParcelas.dataRows.push({
+        ...this.preFormCadastroParcelas.value,
+        nparcelas: index,
+        dataVencimento: dataVencimento,
+      });
+      dataVencimento = moment(dataVencimento)
+        .add(mes, "months")
+        .format("YYYY-MM-DD");
     }
 
     this.preFormCadastroParcelas.reset();
@@ -99,13 +119,13 @@ export class ParcelasComponent implements OnInit {
 
   changeCadastroParcelas(e, row, col) {
     const index = this.tableDataParcelas.dataRows.indexOf(row);
-    this.tableDataParcelas.dataRows[index][col] = col === 'valorNoVencimento' ? e.target.value.slice(2) : e.target.value;
+    this.tableDataParcelas.dataRows[index][col] =
+      col === "valorNoVencimento" ? e.target.value.slice(2) : e.target.value;
   }
 
   incluir() {
     this.incluirParcelas.emit(this.tableDataParcelas.dataRows);
   }
-
 
   deleteRowParcelas(row) {
     const index = this.tableDataParcelas.dataRows.indexOf(row);
@@ -113,11 +133,13 @@ export class ParcelasComponent implements OnInit {
     this.tableDataParcelas.dataRows.splice(index, 1);
     setTimeout(() => {
       this.alertType = {
-        mensagem: 'Registro excluido!',
-        tipo: 'danger'
+        mensagem: "Registro excluido!",
+        tipo: "danger",
       };
-    }, 0)
+    }, 0);
   }
 
-  get pre_form_cadastro_parcelas() { return this.preFormCadastroParcelas.controls; }
+  get pre_form_cadastro_parcelas() {
+    return this.preFormCadastroParcelas.controls;
+  }
 }
