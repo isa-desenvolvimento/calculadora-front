f154d8a7aa568a6690cda4b8f0c8c39c9aba3592
keilane <keilanerizza>
2020-08-17 08:53:43 -0300

    validation to call simularCalc

--- src/app/_component/pages/cheque-empresarial/cheque-empresarial.component.ts
+++ src/app/_component/pages/cheque-empresarial/cheque-empresarial.component.ts
@@ -517,7 +517,7 @@ export class ChequeEmpresarialComponent implements OnInit {
       (chequeEmpresarialList) => {
         this.tableData.dataRows = chequeEmpresarialList
           .filter((row) => row["contractRef"] === infoContrato.contractRef)
-          .map((cheque) => {
+          .map((cheque, index) => {
             cheque.encargosMonetarios = JSON.parse(cheque.encargosMonetarios);
             cheque.infoParaCalculo = JSON.parse(cheque.infoParaCalculo);
             cheque.isTipoLancamento = true;
@@ -534,7 +534,7 @@ export class ChequeEmpresarialComponent implements OnInit {
             });
 
             setTimeout(() => {
-              this.simularCalc(true, null, true);
+              (this.tableData.dataRows.length - 1) === index && this.simularCalc(true, null, true);
             }, 1000);
 
             return cheque;
@@ -550,7 +550,7 @@ export class ChequeEmpresarialComponent implements OnInit {
           return;
         }
 
-        this.tableLoading = false;
+        // this.tableLoading = false;
       },
       (err) => {
         this.alertType = {
