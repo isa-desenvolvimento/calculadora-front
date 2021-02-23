c0daad22e31f6872bbb261660fca2fcdaa11cecf
Andressa <izzaandressa@gmail.com>
2021-02-23 12:26:04 -0300

    change port 3389

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  AUTH_PATH: "https://heroku-api-calculadora.herokuapp.com/auth",
-  API_PATH: "https://heroku-api-calculadora.herokuapp.com/api"
+  // AUTH_PATH: "https://heroku-api-calculadora.herokuapp.com/auth",
+  // API_PATH: "https://heroku-api-calculadora.herokuapp.com/api"
+  AUTH_PATH: "http://safra.t3.eti.br:3389/api/auth",
+  API_PATH: "http://safra.t3.eti.br:3389/api"
 };
